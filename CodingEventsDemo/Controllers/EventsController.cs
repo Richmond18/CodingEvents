dd8ba878b31e9d1cdb60b3d9b5a963131f4b6e26
rashida richmond <richmond1895@gmail.com>
2021-07-27 12:55:11 -0400

    new excrise

--- CodingEventsDemo/Controllers/EventsController.cs
+++ CodingEventsDemo/Controllers/EventsController.cs
@@ -10,17 +10,90 @@ namespace coding_events_practice.Controllers
 {
     public class EventsController : Controller
     {
+        public Dictionary<string, string> eventD = new Dictionary<string, string> { };
+ 
+
         // GET: /<controller>/
-        public IActionResult Index()
+
+        // can i add the dictionary here?
+        //example public string Name {get; srt} = "r";
+        static private IActionResult Index(string Discription, string concert)
         {
             List<string> Events = new List<string>();
-            Events.Add("Code With Pride");
-            Events.Add("Apple WWDC");
+            Events.Add("EDC");
+            Events.Add("COACHELLA");
             Events.Add("Strange Loop");
 
             ViewBag.events = Events;
 
+
+            foreach (var Eventss in ViewBag.Events)
+            {
+                eventD.Add(Eventss, "");
+            }
+
+
+            //ViewBag.eventdis = Discription;
+
+          //  if (concert == "EDC")
+           // {
+                //ViewBag.eventdis = Discription;
+                //ViewBag.eventDic = eventD;
+                //ViewBag.storedD= this.Name;
+
+               // for (int i = 0; i< concert.Length; ++i )
+                //foreach (var compare in eventD)
+                    //if (concert == compare.Key)
+                    if (eventD.ContainsKey("EDC"))
+                    {
+
+                    eventD.Add("keyy", Discription);
+                        // eventD.Remove(compare.Key);
+                        // eventD.Add(compare.Key, Discription);
+                }
+            //}
+
+            ViewBag.eventDd = this.eventD;
             return View();
+
+
+
         }
+
+        public IActionResult EventDiscription()
+       
+
+        {
+
+            //     if (concert == "EDC")
+            //     {
+            //         ViewBag.eventdis = Discription;
+            //         //ViewBag.eventDic = eventD;
+            //         //ViewBag.storedD= this.Name;
+
+            //         foreach (var compare in eventD)
+            //            //if (concert == compare.Key)
+            //            if (eventD.ContainsKey(compare.Key))
+            //                 {
+            //                 eventD.Remove(compare.Key);
+            //                 eventD.Add(compare.Key, Discription);
+            //             }
+
+
+            //     }
+
+            ViewBag.eventDd = this.eventD;
+
+
+            return View();
+
+
+
+        }
+
+
+
     }
+
+
 }
